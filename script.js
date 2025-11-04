4121b76d4d0680ee36f5038f42bcbad3a468e559
codeasobi <carol.tang79@gmail.com>
2025-11-05 00:32:50 +0900

    feat: add scroll smoothly js

--- script.js
+++ script.js
@@ -120,3 +120,14 @@ function applyI18n(lang) {
             }
         });
 }
+
+// scroll smoothly
+document.querySelectorAll('a[href^="#"]').forEach(function (a) {
+    a.addEventListener('click', function (e) {
+        const target = document.querySelector(a.getAttribute('href'));
+        if (target) {
+            e.preventDefault();
+            target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+})
